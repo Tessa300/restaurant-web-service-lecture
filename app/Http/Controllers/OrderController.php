f3377bbe46681e379177ddca304498bce89722fd
Tessa <steinigke300@googlemail.com>
2023-06-02 15:34:04 +0200

    last lecture 24.5.23

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Order;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\HomeController;
 
 class OrderController extends Controller
 {
@@ -20,6 +21,25 @@ class OrderController extends Controller
 
         //dd(DB::table('orders')->select()->get());
 
-        return redirect('home/'.$reservationId)->with('msg', 'Vielen Dank für die Bestellung');
+        return redirect('orders/'.$reservationId)->with('msg', 'Vielen Dank für die Bestellung');
     }
+
+    public function show(int $reservationId){
+        return view('orders', [
+            'reservationId'=>$reservationId, 
+            'orders' => $this->getOrdersByReservationId($reservationId), 
+            'productImageUrl' => 'http://127.0.0.1:8000/images',
+        ]);
+    }
+
+    public static function getOrdersByReservationId(int $reservationId){
+        $orders = DB::table('orders')->select('productId', DB::raw('sum(amount) as amount'))
+                    ->where('reservationId', $reservationId)->groupBy('productId')->get();
+        foreach($orders as $order){
+            $order->product = HomeController::getProduct($order->productId)[0];
+        }
+        return $orders;
+    }
+
+
 }
